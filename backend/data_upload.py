784c26d61f48b2ef66ea8a5b06c43494c6eabc1c
DinisLuis <dinis.luis@tecnico.ulisboa.pt>
2024-02-22 19:03:22 +0000

    subscription data to database with errors

--- backend/data_upload.py
+++ backend/data_upload.py
@@ -11,14 +11,13 @@ def read_json_file(file_path):
     return data
 
 
-def upload_users_to_database(data):
-    '''Function to upload data to the local database'''
+def upload_users_to_database(data, cursor, conn):
+    '''Function to upload users to the local database'''
     insert_sql = """
         INSERT INTO "user" (id, name, email, public_id, phone, account_type, registration_date, status, origin, lang)
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-    #option 1, for cycle
+
     for record in data:
-        
         values = (
             record.get('id', None),
             record.get('name', None),
@@ -36,14 +35,24 @@ def upload_users_to_database(data):
     conn.commit()
 
 
-def upload_subscriptions_to_database(data):
-    '''Function to upload data to the local database'''
-    #option 1, for cycle
+def upload_subscriptions_to_database(data, cursor, conn):
+    '''Function to upload subscriptions to the local database'''
+    insert_sql = """
+        INSERT INTO "subscription" (id, description, category, timestamp, public_id, user_id)
+        VALUES (%s, %s, %s, %s, %s, %s)"""
+    
     for record in data:
-        insert_sql = """
-        INSERT INTO subscription (id, description, category, timestamp, public_id, user_id)
-        VALUES """
-        #cursor.execute(insert_sql, records)
+        values = (
+            record.get('id', None),
+            record.get('description', None),
+            record.get('category', None),
+            record.get('timestamp', None),
+            record.get('public_id', None),
+            record.get('user_id', None)
+        )
+    
+        cursor.execute(insert_sql, values)
+    conn.commit()
 
 #read JSON files located in the /data directory
 subscription = read_json_file('/home/dinis/Desktop/alfredo-fullstack-challenge-2/data/subscriptions.json')
@@ -55,25 +64,17 @@ user = read_json_file('/home/dinis/Desktop/alfredo-fullstack-challenge-2/data/us
 #POSTGRES_USER=admin -e POSTGRES_PASSWORD=thisisatest -e POSTGRES_DB=alfredo -d postgres
 #"postgresql+asyncpg://admin:thisisatest@127.0.0.1:5432/alfredo"
 
-#user_postgres = "admin"
-#passworld_postgres = "thisisatest"
-#db_postgres = "alfredo"
 
 #conn_string = "postgresql+asyncpg://admin:thisisatest@127.0.0.1:5432/alfredo"
 #db = create_engine(conn_string)
 #conn = db.connect()
 
-
-
 conn = psycopg2.connect("postgresql://admin:thisisatest@127.0.0.1:5432/alfredo")
 cursor = conn.cursor()
 
+upload_subscriptions_to_database(subscription, cursor, conn)
 
-upload_users_to_database(user)
-#upload_subscriptions_to_database(subscription)
-
-
-
+upload_users_to_database(user, cursor, conn)
 
 conn.close()
 #df.to_sql('data', con=conn, if_exists = 'replace', index=False)
