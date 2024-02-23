2841530d4567df13c65c3190191048e644c8815d
DinisLuis <dinis.luis@tecnico.ulisboa.pt>
2024-02-23 15:37:07 +0000

    endpoint configuration

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,32 @@
 from fastapi import FastAPI
+from flask import *
 
+import json
 
 app = FastAPI(title = "Alfredo Fullstack Challenge")
 
 
 @app.get("/complaints")
 async def get_complaints():
+    #data_set = {'Page': 'Complaints', 'Message': 'Successfully loaded the Complaints page'}
+    #json_dump = json.dumps(data_set)
+
+    #return json_dump
     return []
+
+@app.get('/cancelation_reasons') #, methods = ['GET']
+async def cancelation_reasons(time_window = None):
+    if (time_window == "1year"):
+        time_window = "ONE YEAR"
+    elif (time_window == "1month"):
+        time_window = "ONE MONTH"
+    elif (time_window == "1week"):
+        time_window = "ONE WEEK"
+    else:
+        time_window = "None"
+
+    data_set = {'Page': 'Cancelation Reasons', 'Message': f'Successfully got the request for {time_window}'}
+    json_dump = json.dumps(data_set)
+
+    return json_dump
+    #return{"time_window": time_window}
